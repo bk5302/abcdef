6e1e14f4fcb5aa8ce46b964ac78d973b17224e70
bk5302 <b5302b@naver.com>
2015-03-29 20:42:35 +0900

    망함

---
+++ TestConsole/Source1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+int main()
+{
+	int card[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	char card[4] = { k, q, j };
+	srand(time(NULL));
+	int card[] = (rand() % 11);
+	printf("%c", card);
+	for (;;);
+
+
+
+
+}
--- TestConsole/TestConsole/Source.cpp
+++ TestConsole/TestConsole/Source.cpp
@@ -1,16 +1,85 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <Windows.h>
+
 int main()
 {
-	int card[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9};
-	char card[4] = { k, q, j };
-	srand(time(NULL));
-	int card[] = (rand() % 11);
-	printf("%c", card);
-	for (;;);
+	
+		int a, b, c, d;
+		int p1, p2;
+		char yorn;
+		int diller;
+		int cnum1 = 2, cnum2 = 2;
+		printf("게임시작\n");
+		printf("우선 각각 두장의 카드를 받습니다.\n");
+		system("pause");
+		system("cls");
+		srand(time(NULL));
+		a = (rand() % 11) + 1;
+		b = (rand() % 11) + 1;
+		printf("딜러 안보여줌%d,나 %d\n", a, b);
+		c = (rand() % 11) + 1;
+		d = (rand() % 11) + 1;
+		printf("딜러 %d,나 %d\n", c, d);
+		p1 = a + c;
+		p2 = b + d;
+		while (1)
+		{
+		if (p1 < 21 && p2 < 21)
+		if (cnum1 != 5, cnum2 != 5)
+		{
+		
+		if (p1 > 16)
+			diller = (rand() % 2) + 1;
+		if (diller == 1)//딜러카드받을지말지
+		{
+			
+			printf("딜러 %d\n", c);
+			cnum1++;
+			p1 = p1 + c;
+		}
+		printf("카드를받으시겠습니까? y or n\n");
+		scanf("%c", &yorn);
+		switch (yorn)
+		{
+
+		case 'y':
+		case 'Y':
+		d = (rand() % 11) + 1;
+		printf("나 %d", d);
+		p2 = p2 + d;
+		cnum2++;
+		break;
+		case 'n':
+		case 'N':
+			printf("카드를 안받았습니다");
+			printf("게임을 끝내시겠습니까");
+			{
+
+			}
+		break;
+
+		}
+		if (p1 > 21 || p2 > 21)
+		{
+			if (p1 > 21)
+			{
+				printf("내가이김");
+			}
+			if (p2 > 21)
+			{
+				printf("내가짐");
+			}
+		}
+			if (cnum1 == 5 && cnum2 == 5)
+			{
+
+			}
+
+		
 
 
-
-
+		}
+	}
 }
